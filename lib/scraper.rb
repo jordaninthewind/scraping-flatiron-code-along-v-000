e985ff7d8dee0cf45b1075a2e9d39b659d1696bf
Jordan Kline <jordan.kline@gmail.com>
2018-01-12 02:40:02 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,17 +6,6 @@ require_relative './course.rb'
 
 class Scraper
 
-  def print_courses
-    self.make_courses
-    Course.all.each do |course|
-      if course.title
-        puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
-        puts "  Description: #{course.description}"
-      end
-    end
-  end
-
   def get_courses
     self.get_page.css(".post")
   end
@@ -29,6 +18,17 @@ class Scraper
       course.title = post.css("h2").text
       course.schedule = post.css(".date").text
       course.description = post.css("p").text
+    end
+  end
+  
+  def print_courses
+    self.make_courses
+    Course.all.each do |course|
+      if course.title
+        puts "Title: #{course.title}"
+        puts "  Schedule: #{course.schedule}"
+        puts "  Description: #{course.description}"
+      end
     end
   end
 
